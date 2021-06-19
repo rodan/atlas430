0ed9481690340a5fa6b8a9484ff9382d2b5b2934
Petre Rodan <c59ccef70aa1387612c4@subdimension.ro>
2021-06-19 13:37:27 +0300

    add submodules

--- tests/uart0/version.h
+++ tests/uart0/version.h
@@ -1,9 +1,9 @@
 #ifndef __VERSION_H__
 #define __VERSION_H__
 
-// compiled on Sat Jun 19 07:47:26 UTC 2021
+// compiled on Sat Jun 19 10:33:30 UTC 2021
 
-#define BUILD 226
-#define COMMIT 87
+#define BUILD 227
+#define COMMIT 27
 
 #endif
