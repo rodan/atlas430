0e97de895bd85500240a04b567b24ddd9b6b36a9
Petre Rodan <c59ccef70aa1387612c4@subdimension.ro>
2021-06-19 23:37:53 +0300

    Makefile targets are now ifdefs

--- tests/uart0/version.h
+++ tests/uart0/version.h
@@ -1,9 +1,9 @@
 #ifndef __VERSION_H__
 #define __VERSION_H__
 
-// compiled on Sat Jun 19 12:11:13 UTC 2021
+// compiled on Sat Jun 19 20:32:41 UTC 2021
 
-#define BUILD 233
-#define COMMIT 28
+#define BUILD 248
+#define COMMIT 29
 
 #endif
