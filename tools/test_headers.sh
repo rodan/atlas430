e55f8bca193429e7474b01d030dab023e6ca9285
Petre Rodan <2b4eda@subdimension.ro>
2021-11-12 08:21:03 +0200

    add header

--- tools/test_headers.sh
+++ tools/test_headers.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 
+# script that checks all fram-enabled uCs for __MSP430_HAS_FRAM__
+# and reports headers that failed this criteria
+#
+# source:    https://github.com/rodan/reference_libs_msp430
+# author:    Petre Rodan <2b4eda@subdimension.ro>
+# license:   BSD
+
+
 # you might need to tweak these 3 locations
 CC='/opt/msp430/bin/msp430-elf-gcc'
 SUPPORT_FILES='/opt/msp430-gcc-support-files/include'
@@ -9,8 +17,8 @@ list_of_ucs=$(ls -1 ${SUPPORT_FILES}/*430fr*.h | sed 's|\.h$||;s|.*include/||' |
 
 create_main()
 {
-mkdir -p "${TEMP_DIR}"
-cat << EOF > "${TEMP_DIR}/main.c"
+    mkdir -p "${TEMP_DIR}"
+    cat << EOF > "${TEMP_DIR}/main.c"
 #include <msp430.h>
 
 int main(void)
