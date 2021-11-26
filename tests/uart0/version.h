041604410ec7b8c2498dc449dce579c03dc17436
Petre Rodan <2b4eda@subdimension.ro>
2021-11-26 13:59:54 +0200

    fr2xx-4xx uart test passed

--- tests/uart0/version.h
+++ tests/uart0/version.h
@@ -1,9 +1,9 @@
 #ifndef __VERSION_H__
 #define __VERSION_H__
 
-// compiled on Fri Nov 26 08:42:23 UTC 2021
+// compiled on Fri Nov 26 11:55:52 UTC 2021
 
-#define BUILD 33
-#define COMMIT 109
+#define BUILD 40
+#define COMMIT 111
 
 #endif
