def746dccb6737bb7953f8a493ffad1d838bac3a
Petre Rodan <2b4eda@subdimension.ro>
2023-01-30 07:15:53 +0200

    cleanup

--- glue/generic/sch.c
+++ glue/generic/sch.c
@@ -13,85 +13,58 @@
 
 #include "sig.h"
 
-sch_descriptor *sch_t0a_d;
-sch_descriptor *sch_t1a_d;
-sch_descriptor *sch_t2a_d;
+static sch_descriptor schd;
 
-uint8_t sch_init(sch_descriptor * schd)
+uint8_t sch_init(void)
 {
-
-    // configure TIMER subsystem based on baseAddress
-    switch (schd->baseAddress) {
-#if defined (TIMER_A0_BASE)
-    case TIMER_A0_BASE:
-        sch_t0a_d = schd;
-        break;
-#endif
-#if defined (TIMER_A1_BASE)
-    case TIMER_A1_BASE:
-        sch_t1a_d = schd;
-        break;
-#endif
-#if defined (TIMER_A2_BASE)
-    case TIMER_A2_BASE:
-        sch_t2a_d = schd;
-        break;
-#endif
-    default:
-        return EXIT_FAILURE;
-        break;
-    }
-
     __disable_interrupt();
-    HWREG16(schd->baseAddress + OFS_TAxCTL) = TASSEL__ACLK + MC__CONTINOUS + TACLR;
-    HWREG16(schd->baseAddress + OFS_TAxEX0) = 0;
+    HAL_SCH_TAxCTL = TASSEL__ACLK + MC__CONTINOUS + TACLR;
+    HAL_SCH_TAxEX0 = 0;
     __enable_interrupt();
 
-    // ccr1 will count up to 10ms
-    HWREG16(schd->baseAddress + OFS_TAxCCTL1) = 0;
-    HWREG16(schd->baseAddress + OFS_TAxCCR1) = HWREG16(schd->baseAddress + OFS_TAxR) + SCH_TICK;
-    HWREG16(schd->baseAddress + OFS_TAxCCTL1) = CCIE;
+    // ccr1 will count up to ~10ms
+    HAL_SCH_TAxCCTL1 = 0;
+    HAL_SCH_TAxCCR1 = HAL_SCH_TAxR + SCH_TICK;
+    HAL_SCH_TAxCCTL1 = CCIE;
 
-    schd->systime = 0;
-    schd->trigger_schedule = 0;
-    schd->trigger_next = 0;
-    schd->last_event = SCH_EVENT_NONE;
-    schd->systime = 0;
+    schd.systime = 0;
+    schd.trigger_schedule = 0;
+    schd.trigger_next = 0;
+    schd.last_event = SCH_EVENT_NONE;
+    schd.systime = 0;
 
     return EXIT_SUCCESS;
 }
 
 // slot is between 0 and SCH_SLOTS_COUNT-1 inclusive
-uint8_t sch_set_trigger_slot(sch_descriptor * schd, const uint16_t slot, const uint32_t trigger,
-                             const uint8_t flag)
+uint8_t sch_set_trigger_slot(const uint16_t slot, const uint32_t trigger, const uint8_t flag)
 {
     if (slot > SCH_SLOTS_COUNT - 1) {
         return EXIT_FAILURE;
     }
 
-    schd->trigger_slot[slot] = trigger;
+    schd.trigger_slot[slot] = trigger;
     if (flag == SCH_EVENT_ENABLE) {
-        schd->trigger_schedule |= (1UL << slot);
+        schd.trigger_schedule |= (1UL << slot);
 
-        if (schd->trigger_next > trigger) {
-            schd->trigger_next = trigger;
+        if (schd.trigger_next > trigger) {
+            schd.trigger_next = trigger;
         }
     } else {
-        schd->trigger_schedule &= ~(1UL << slot);
+        schd.trigger_schedule &= ~(1UL << slot);
     }
 
     return EXIT_SUCCESS;
 }
 
-uint8_t sch_get_trigger_slot(const sch_descriptor * schd, const uint16_t slot, uint32_t * trigger,
-                             uint8_t * flag)
+uint8_t sch_get_trigger_slot(const uint16_t slot, uint32_t * trigger, uint8_t * flag)
 {
     if (slot > SCH_SLOTS_COUNT - 1) {
         return EXIT_FAILURE;
     }
 
-    *trigger = schd->trigger_slot[slot];
-    if (schd->trigger_schedule & (1UL << slot)) {
+    *trigger = schd.trigger_slot[slot];
+    if (schd.trigger_schedule & (1UL << slot)) {
         *flag = 1;
     } else {
         *flag = 0;
@@ -100,158 +73,90 @@ uint8_t sch_get_trigger_slot(const sch_descriptor * schd, const uint16_t slot, u
     return EXIT_SUCCESS;
 }
 
-uint16_t sch_get_event(const sch_descriptor * schd)
+uint16_t sch_get_event(void)
 {
-    return schd->last_event;
+    return schd.last_event;
 }
 
-void sch_rst_event(sch_descriptor * schd)
+void sch_rst_event(void)
 {
-    schd->last_event = SCH_EVENT_NONE;
+    schd.last_event = SCH_EVENT_NONE;
 }
 
-uint16_t sch_get_event_schedule(const sch_descriptor * schd)
+uint16_t sch_get_event_schedule(void)
 {
-    return schd->last_event_schedule;
+    return schd.last_event_schedule;
 }
 
-void sch_rst_event_schedule(sch_descriptor * schd)
+void sch_rst_event_schedule(void)
 {
-    schd->last_event_schedule = SCH_EVENT_NONE;
+    schd.last_event_schedule = SCH_EVENT_NONE;
 }
 
-uint32_t sch_get_trigger_next(const sch_descriptor * schd)
+uint32_t sch_get_trigger_next(void)
 {
-    return schd->trigger_next;
+    return schd.trigger_next;
 }
 
-uint32_t systime(const sch_descriptor * schd)
+uint32_t systime(void)
 {
-    return schd->systime;
+    return schd.systime;
 }
 
-void sch_handler(sch_descriptor * schd)
+void sch_handler(void)
 {
     uint16_t c;
     uint16_t shift;
     uint32_t schedule_trigger_next = 0;
 
-    if (schd->trigger_schedule) {
+    if (schd.trigger_schedule) {
         for (c = 0; c < SCH_SLOTS_COUNT; c++) {
             shift = 1UL << c;
-            if (schd->trigger_schedule & shift) {
+            if (schd.trigger_schedule & shift) {
                 // signal if event time was reached
-                if (schd->trigger_slot[c] <= schd->systime) {
-                    schd->trigger_schedule &= ~shift;
-                    schd->last_event_schedule |= shift;
+                if (schd.trigger_slot[c] <= schd.systime) {
+                    schd.trigger_schedule &= ~shift;
+                    schd.last_event_schedule |= shift;
                 } else
                     // prepare the next trigger
-                if (schd->trigger_slot[c] < schedule_trigger_next) {
-                    schedule_trigger_next = schd->trigger_slot[c];
+                if (schd.trigger_slot[c] < schedule_trigger_next) {
+                    schedule_trigger_next = schd.trigger_slot[c];
                 }
             }
         }
-        schd->trigger_next = schedule_trigger_next;
+        schd.trigger_next = schedule_trigger_next;
     }
 }
 
-#ifdef SCH_USES_T0A
+#if defined(SCH_USES_T0A) || defined(SCH_USES_T1A) || defined(SCH_USES_T2A)
 
 #if defined(__TI_COMPILER_VERSION__) || defined(__IAR_SYSTEMS_ICC__)
-#pragma vector=HAL_TIMER0A_VECTOR
-__interrupt void TIMER0A_ISR(void)
+#pragma vector=HAL_SCH_TIMER_VECTOR
+__interrupt void SCH_TIMER_ISR(void)
 #elif defined(__GNUC__)
-void __attribute__((interrupt(HAL_TIMER0A_VECTOR))) TIMER0A_ISR(void)
+void __attribute__((interrupt(HAL_SCH_TIMER_VECTOR))) SCH_TIMER_ISR(void)
 #else
 #error Compiler not supported!
 #endif
 {
-    uint16_t iv = TA0IV;
+    uint16_t iv = HAL_SCH_TAxIV;
 
-    if (iv == TAIV__TACCR1) {
+    if (iv == HAL_SCH_TAxIV_TACCR1) {
         // timer used by timer_a1_delay_noblk_ccr1()
         // disable interrupt
-        TA0CCTL1 &= ~CCIE;
-        TA0CCTL1 = 0;
-        TA0CCR1 = TA0R + SCH_TICK;
-        TA0CCTL1 = CCIE;
-        sch_systime++;
+        HAL_SCH_TAxCCTL1 &= ~CCIE;
+        HAL_SCH_TAxCCTL1 = 0;
+        HAL_SCH_TAxCCR1 = HAL_SCH_TAxR + SCH_TICK;
+        HAL_SCH_TAxCCTL1 = CCIE;
+        schd.systime++;
 
-        if (sch_t0a_d->trigger_next <= sch_t0a_d->systime) {
-            sch_t0a_d->last_event |= SCH_EVENT_CCR1;
+        if (schd.trigger_next <= schd.systime) {
+            schd.last_event |= SCH_EVENT_CCR1;
             _BIC_SR_IRQ(LPM3_bits);
         }
-    } else if (iv == TA0IV_TA0IFG) {
+    } else if (iv == HAL_SCH_TAxIV_TAxIFG) {
         // overflow
-        TA0CTL &= ~TAIFG;
-    }
-}
-
-#endif
-
-#ifdef SCH_USES_T1A
-
-#if defined(__TI_COMPILER_VERSION__) || defined(__IAR_SYSTEMS_ICC__)
-#pragma vector=HAL_TIMER1A_VECTOR
-__interrupt void TIMER1A_ISR(void)
-#elif defined(__GNUC__)
-void __attribute__((interrupt(HAL_TIMER1A_VECTOR))) TIMER1A_ISR(void)
-#else
-#error Compiler not supported!
-#endif
-{
-    uint16_t iv = TA1IV;
-
-    if (iv == HAL_TA1IV_TACCR1) {
-        // timer used by timer_a1_delay_noblk_ccr1()
-        // disable interrupt
-        TA1CCTL1 &= ~CCIE;
-        TA1CCTL1 = 0;
-        TA1CCR1 = TA1R + SCH_TICK;
-        TA1CCTL1 = CCIE;
-        sch_t1a_d->systime++;
-
-        if (sch_t1a_d->trigger_next <= sch_t1a_d->systime) {
-            sch_t1a_d->last_event |= SCH_EVENT_CCR1;
-            _BIC_SR_IRQ(LPM3_bits);
-        }
-    } else if (iv == TA1IV_TA1IFG) {
-        // overflow
-        TA1CTL &= ~TAIFG;
-    }
-}
-
-#endif
-
-#ifdef SCH_USES_T2A
-
-#if defined(__TI_COMPILER_VERSION__) || defined(__IAR_SYSTEMS_ICC__)
-#pragma vector=HAL_TIMER2A_VECTOR
-__interrupt void TIMER2A_ISR(void)
-#elif defined(__GNUC__)
-void __attribute__((interrupt(HAL_TIMER2A_VECTOR))) TIMER2A_ISR(void)
-#else
-#error Compiler not supported!
-#endif
-{
-    uint16_t iv = TA2IV;
-
-    if (iv == TAIV__TACCR1) {
-        // timer used by timer_a1_delay_noblk_ccr1()
-        // disable interrupt
-        TA2CCTL1 &= ~CCIE;
-        TA2CCTL1 = 0;
-        TA2CCR1 = TA2R + SCH_TICK;
-        TA2CCTL1 = CCIE;
-        sch_systime++;
-
-        if (sch_t2a_d->trigger_next <= sch_t2a_d->systime) {
-            sch_t2a_d->last_event |= SCH_EVENT_CCR1;
-            _BIC_SR_IRQ(LPM3_bits);
-        }
-    } else if (iv == TA2IV_TA2IFG) {
-        // overflow
-        TA2CTL &= ~TAIFG;
+        HAL_SCH_TAxCTL &= ~TAIFG;
     }
 }
 
--- glue/generic/sch.h
+++ glue/generic/sch.h
@@ -50,85 +50,102 @@
 
 #ifdef SCH_USES_T0A
     #if defined (TIMER0_A1_VECTOR)
-        #define HAL_TIMER0A_VECTOR TIMER0_A1_VECTOR
+        #define HAL_SCH_TIMER_VECTOR TIMER0_A1_VECTOR
     #else
         #error TIMER0_A1 interrupt vector name unknown
     #endif
 
     #if defined (TA0IV__TACCR1)
-        #define HAL_TA0IV_TACCR1 TA0IV__TACCR1
+        #define HAL_SCH_TAxIV_TACCR1 TA0IV__TACCR1
     #elif defined (TA0IV_TACCR1)
-        #define HAL_TA0IV_TACCR1 TA0IV_TACCR1
+        #define HAL_SCH_TAxIV_TACCR1 TA0IV_TACCR1
     #else
         #error TA0IV_TACCR1 interrupt vector register name unknown
     #endif
+
+    #define HAL_SCH_TAxIV TA0IV
+    #define HAL_SCH_TAxCCTL1 TA0CCTL1
+    #define HAL_SCH_TAxCCR1 TA0CCR1
+    #define HAL_SCH_TAxR TA0R
+    #define HAL_SCH_TAxCTL TA0CTL
+    #define HAL_SCH_TAxIV_TAxIFG TA0IV_TA0IFG
+    #define HAL_SCH_TAxEX0 TA0EX0
 #endif
 
 #ifdef SCH_USES_T1A
     #if defined (TIMER1_A1_VECTOR)
-        #define HAL_TIMER1A_VECTOR TIMER1_A1_VECTOR
+        #define HAL_SCH_TIMER_VECTOR TIMER1_A1_VECTOR
     #else
         #error TIMER1_A1 interrupt vector name unknown
     #endif
 
     #if defined (TA1IV__TACCR1)
-        #define HAL_TA1IV_TACCR1 TA1IV__TACCR1
+        #define HAL_SCH_TAxIV_TACCR1 TA1IV__TACCR1
     #elif defined (TA1IV_TACCR1)
-        #define HAL_TA1IV_TACCR1 TA1IV_TACCR1
+        #define HAL_SCH_TAxIV_TACCR1 TA1IV_TACCR1
     #else
         #error TA1IV_TACCR1 interrupt vector register name unknown
     #endif
+
+    #define HAL_SCH_TAxIV TA1IV
+    #define HAL_SCH_TAxCCTL1 TA1CCTL1
+    #define HAL_SCH_TAxCCR1 TA1CCR1
+    #define HAL_SCH_TAxR TA1R
+    #define HAL_SCH_TAxCTL TA1CTL
+    #define HAL_SCH_TAxIV_TAxIFG TA1IV_TA1IFG
+    #define HAL_SCH_TAxEX0 TA1EX0
 #endif
 
 #ifdef SCH_USES_T2A
     #if defined (TIMER2_A1_VECTOR)
-        #define HAL_TIMER2A_VECTOR TIMER2_A1_VECTOR
+        #define HAL_SCH_TIMER_VECTOR TIMER2_A1_VECTOR
     #else
         #error TIMER2_A1 interrupt vector name unknown
     #endif
 
     #if defined (TA2IV__TACCR1)
-        #define HAL_TA2IV_TACCR1 TA2IV__TACCR1
+        #define HAL_SCH_TAxIV_TACCR1 TA2IV__TACCR1
     #elif defined (TA2IV_TACCR1)
-        #define HAL_TA2IV_TACCR1 TA2IV_TACCR1
+        #define HAL_SCH_TAxIV_TACCR1 TA2IV_TACCR1
     #else
         #error TA2IV_TACCR1 interrupt vector register name unknown
     #endif
+
+    #define HAL_SCH_TAxIV TA2IV
+    #define HAL_SCH_TAxCCTL1 TA2CCTL1
+    #define HAL_SCH_TAxCCR1 TA2CCR1
+    #define HAL_SCH_TAxR TA2R
+    #define HAL_SCH_TAxCTL TA2CTL
+    #define HAL_SCH_TAxIV_TAxIFG TA2IV_TA2IFG
+    #define HAL_SCH_TAxEX0 TA2EX0
 #endif
 
-
-///< struct who's pointer is passed arround during most calls present in this HAL implementation
-// do not read/write to the structure elements directly, try to treat this struct as opaque data
-// baseAddress is the only variable excepted from the above rule. it needs to be 
-// set before calling sch_init()
-// also if you write your own RX interrupt handler callback all elements can be directly read and written
 typedef struct _sch_descriptor {
-    uint16_t baseAddress;           ///< timer subsystem offset base register
     uint16_t trigger_schedule;  ///< bitwise flag of which slots are currently active
     uint32_t trigger_next;      ///< tick when this scheduler is supposed to trigger next
     uint32_t trigger_slot[SCH_SLOTS_COUNT]; ///< systimes at which slots are set to trigger
     uint16_t last_event;        ///< bitwise flag of current timer_a2 interrupt events
     uint16_t last_event_schedule;        ///< bitwise flag of which slots have currently triggered
-    uint32_t systime;           ///< time in 10ms increments since scheduler started
+    uint32_t systime;           ///< time in ticks (10ms increments) since scheduler started
 } sch_descriptor;
 
 #ifdef __cplusplus
 extern "C" {
 #endif
 
-uint8_t sch_init(sch_descriptor *schd);
+uint8_t sch_init(void);
 
-uint16_t sch_get_event(const sch_descriptor *schd);
-void sch_rst_event(sch_descriptor *schd);
-uint16_t sch_get_event_schedule(const sch_descriptor *schd);
-void sch_rst_event_schedule(sch_descriptor *schd);
-uint32_t sch_get_trigger_next(const sch_descriptor *schd);
+uint16_t sch_get_event(void);
+void sch_rst_event(void);
+uint16_t sch_get_event_schedule(void);
+void sch_rst_event_schedule(void);
+uint32_t sch_get_trigger_next(void);
 
-uint32_t systime(const sch_descriptor *schd);
+uint32_t systime(void);
 
-uint8_t sch_set_trigger_slot(sch_descriptor *schd, const uint16_t slot, const uint32_t trigger, const uint8_t flag);
-uint8_t sch_get_trigger_slot(const sch_descriptor *schd, const uint16_t slot, uint32_t *trigger, uint8_t *flag);
-void sch_handler(sch_descriptor *schd);
+uint8_t sch_set_trigger_slot(const uint16_t slot, const uint32_t trigger, const uint8_t flag);
+uint8_t sch_get_trigger_slot(const uint16_t slot, uint32_t *trigger, uint8_t *flag);
+void sch_handler(void);
 
 #ifdef __cplusplus
 }
