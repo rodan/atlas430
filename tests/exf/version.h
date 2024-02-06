e86e3a4c7755659bd8e9aa8830b7540a15d1fba8
Petre Rodan <petre.rodan@subdimension.ro>
2024-02-06 12:47:32 +0200

    Makefile: fix grep warning about stray '\'

--- tests/exf/version.h
+++ tests/exf/version.h
@@ -1,9 +1,9 @@
 #ifndef __VERSION_H__
 #define __VERSION_H__
 
-// compiled on Mon Aug 29 14:15:11 UTC 2022
+// compiled on Mon Jun 12 02:17:00 UTC 2023
 
-#define BUILD 53
-#define COMMIT 164
+#define BUILD 54
+#define COMMIT 170
 
 #endif
