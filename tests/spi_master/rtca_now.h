f3dfe99af568d6aa2dab7f32214616d9b236df58
Petre Rodan <2b4eda@subdimension.ro>
2024-12-18 08:17:40 +0200

    sys -> arch move

--- tests/spi_master/rtca_now.h
+++ tests/spi_master/rtca_now.h
@@ -4,9 +4,9 @@
 
 #define COMPILE_YEAR 2024
 #define COMPILE_MON 12
-#define COMPILE_DAY 17
-#define COMPILE_DOW 2
-#define COMPILE_HOUR 17
-#define COMPILE_MIN 44
+#define COMPILE_DAY 18
+#define COMPILE_DOW 3
+#define COMPILE_HOUR 8
+#define COMPILE_MIN 12
 
 #endif
